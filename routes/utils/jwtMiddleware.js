abc63f12f27f6d17c83bdb2f0f8560c2c2ae35a7
spaceforestchu <spaceforestchu@gmail.com>
2021-07-07 16:09:22 -0400

    add jwt-middleware

--- routes/utils/jwtMiddleware.js
+++ routes/utils/jwtMiddleware.js
@@ -4,7 +4,7 @@ async function checkJwtToken(req, res, next) {
   try {
     if (req.headers && req.headers.authorization) {
       // console.log(req.headers);
-      // console.log(req.headers.authorization);
+      //console.log(req.headers.authorization);
       let jwtToken = req.headers.authorization.slice(7);
 
       let decodedJwt = jwt.verify(jwtToken, process.env.PRIVATE_JWT_KEY);
